03ec68356e7d8a4278672375410bc75c019103eb
Lethabo Kgoele <lethabotshepojr@gmail.com>
2025-06-26 09:07:38 +0200

    made the tab button get current chrome tab and input button

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 if (!myLeads) {
   myLeads = [];
@@ -11,13 +12,6 @@ if (!myLeads) {
     renderLeads(lead);
   }
 }
-
-inputBtn.addEventListener("click", () => {
-  myLeads.push(inputEl.value);
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads(inputEl.value);
-  inputEl.value = "";
-});
 
 function renderLeads(lead) {
   const listItems = `
@@ -30,8 +24,24 @@ function renderLeads(lead) {
   ulEl.innerHTML += listItems;
 }
 
+inputBtn.addEventListener("click", () => {
+  myLeads.push(inputEl.value);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads(inputEl.value);
+  inputEl.value = "";
+});
+
 deleteBtn.addEventListener("dblclick", () => {
   localStorage.clear();
   myLeads = [];
   ulEl.innerHTML = "";
+});
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let activeTabUrl = tabs[0].url;
+    myLeads.push(activeTabUrl);
+    renderLeads(activeTabUrl);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  });
 });
